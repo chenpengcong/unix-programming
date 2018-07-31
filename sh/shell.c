f00e925bedc4203e67545e279b4386d02a65468e
chenpengcong <chenpengcong@gmail.com>
2018-07-31 16:04:55 +0800

    set RA_RESTART flag

--- sh/shell.c
+++ sh/shell.c
@@ -5,22 +5,34 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <signal.h>
 
 #define MAX_ARGC 10
 char* new_str(char *str);
+void int_handler(int signum);
+
 int main()
 {
     char *args[MAX_ARGC];
+    struct sigaction oldact;
     char cmd[20];
     char buf[20];
     char index = 0;
     int i = 0;
     pid_t pid;
+    //signal(SIGINT, int_handler);
+    sigaction(SIGINT, NULL, &oldact);
+    oldact.sa_flags &= ~(SA_RESTART | SA_SIGINFO);//取消SA_RESTART标志，让阻塞的fgets直接失败返回
+    oldact.sa_handler = int_handler;
+    sigaction(SIGINT, &oldact, NULL);
     while (1) {
         index = 0;
         printf("$ ");
         if (fgets(buf, 20, stdin) && buf[0] != '\n') {
             args[index++] = new_str(buf);
+            if (strncmp(args[0], "exit", 4) == 0) {
+                exit(0); 
+            }
         } else {
             continue;
         }
@@ -58,4 +70,9 @@ char* new_str(char *str)
     new_s = (char *)malloc(len);
     memcpy(new_s, str, len);
     return new_s;
+}
+
+void int_handler(int signum)
+{
+    printf("receive int signal\n");
 }
